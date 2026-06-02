bbf5333ed9f93271a087d1b14b2377d6442279c5
Aachal <aachalshresth@gmail.com>
2026-06-02 16:53:04 +0200

    adjust viteconfig

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import basicSsl from '@vitejs/plugin-basic-ssl'
-console.log('VITE CONFIG LOADED')
+import { resolve } from 'path'
+
 export default defineConfig({
   plugins: [basicSsl()],
-  server: {
-    https: true,
-    host: true,
-    allowedHosts: ['localhost', 'brandon.trycloudflare.com', 'trycloudflare.com'],
-  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        rice: resolve(__dirname, 'rice.html'),
+        info: resolve(__dirname, 'info.html'),
+      }
+    }
+  }
 })
